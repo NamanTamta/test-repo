d1a56a6c9a190e7460a527bd5b266bfcaf091add
Naman Tamta <namantamta987@gmail.com>
2025-10-05 12:09:58 +0530

    Update

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 print("Hello, World!")
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*
+# This is a sample Python script.
+print("Hello, World!")  
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and 
